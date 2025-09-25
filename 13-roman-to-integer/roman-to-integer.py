5279f9246882fb9dd069e930dc814d7fd76eb852
MAROTHU CHANDRA SEKHAR <167837257+MAROTHUCHANDRASEKHAR@users.noreply.github.com>
2025-09-25 22:38:41 +0530

    Time: 5 ms (52.08%) | Memory: 17.9 MB (20.34%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,41 +1,13 @@
 class Solution:
-    def romanToInt(self, s):
-        ret_val = 0
-
-        sym_to_val_map = {}
-        sym_to_val_map['I'] = 1
-        sym_to_val_map['V'] = 5
-        sym_to_val_map['X'] = 10
-        sym_to_val_map['L'] = 50
-        sym_to_val_map['C'] = 100
-        sym_to_val_map['D'] = 500
-        sym_to_val_map['M'] = 1000
-
-        i = 0
-        while (i < len(s)):
-            c = s[i]
-            c2 = s[i:i+2]
-
-            if (c2 == 'IV'):
-                ret_val += 4
-                i += 2
-            elif (c2 == 'IX'):
-                ret_val += 9
-                i += 2
-            elif (c2 == 'XL'):
-                ret_val += 40
-                i += 2
-            elif (c2 == 'XC'):
-                ret_val += 90
-                i += 2
-            elif (c2 == 'CD'):
-                ret_val += 400
-                i += 2
-            elif (c2 == 'CM'):
-                ret_val += 900
-                i += 2
+    def romanToInt(self, s: str) -> int:
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                result -= roman_to_int[s[i]]
             else:
-                ret_val += sym_to_val_map[c]
-                i += 1
-            
-        return ret_val
+                result += roman_to_int[s[i]]
+        return result
